77e7b3e723112d26c48e3917fc7dd7d7f8e0ff94
oasisgorilla <yongjae0522@gmail.com>
2025-08-20 13:51:01 +0900

    type에 맞는 모듈 import

--- scraper/__init__.py
+++ scraper/__init__.py
@@ -1,6 +1,9 @@
 # scraper/__init__.py
-from typing import Literal
+from typing import Literal, TYPE_CHECKING
 from log_util import setup_logger
+
+if TYPE_CHECKING:
+    from scraper.law_scraper import LawScraper
 
 def make_law_scraper(mode: Literal["central", "local"], **kwargs) -> "LawScraper":
     """법령 스크래퍼 팩토리 함수"""
